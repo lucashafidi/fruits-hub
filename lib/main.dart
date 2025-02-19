39da41ececee9451f471bac3755742ca12a57ccf
lucashafidi <89469272+lucashafidi@users.noreply.github.com>
2025-02-19 21:14:34 +0100

    First Commit
    First Date : 02/03/2025

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
